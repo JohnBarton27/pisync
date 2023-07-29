435b4e255dddccd0ed9a3f15c7e3c2ea3f08a312
John Barton <john@mediayoucanfeel.com>
2023-07-29 16:54:46 -0400

    Playing media remotely works!

--- client.py
+++ client.py
@@ -42,8 +42,15 @@ def connect_to_server():
     server_ip = '192.168.1.115'  # TODO remove hardcoded server IP
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, settings.SOCKET_PORT))
-    print('Connected to the server:', server_ip, settings.SOCKET_PORT)
+
+    while not stop_flag.is_set():
+        try:
+            client_socket.connect((server_ip, settings.SOCKET_PORT))
+            print('Connected to the server:', server_ip, settings.SOCKET_PORT)
+            break
+        except ConnectionRefusedError:
+            print("Unable to hit server...")
+            time.sleep(2)
 
     # Send Media objects to server
     media_objs = Media.get_all_from_db()
@@ -64,10 +71,11 @@ def connect_to_server():
                 break
 
             message_obj = Message.get_from_socket(data)
-            print(message_obj)
-
-    # If disconnected, try to reconnect
-    connect_to_server()
+            if isinstance(message_obj, MediaPlayRequestMessage):
+                filepath_of_media_to_play = message_obj.get_content()
+                for media in Media.get_all_from_db():
+                    if media.file_path == filepath_of_media_to_play:
+                        media.play()
 
     recv_thread = threading.Thread(target=receive_server_messages)
     recv_thread.start()
