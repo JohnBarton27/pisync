67019b2819f531b22184b0a8730c7667b2a58288
John Barton <john@mediayoucanfeel.com>
2023-09-04 16:17:58 -0400

    #6 Add better handling for client to receive larger messages (like file uploads)

--- pisync/socket_handlers/client.py
+++ pisync/socket_handlers/client.py
@@ -44,7 +44,8 @@ def connect_to_server(app):
     opening_message = ClientMediaDumpMessage(media_pickle)
     opening_message.send(client_socket)
 
-    def receive_server_messages():
+    def get_message_from_socket(server_socket):
+        data_pieces = []
         while not app.stop_flag.is_set():
             data = client_socket.recv(1024)
             if not data:
@@ -53,30 +54,39 @@ def connect_to_server(app):
                 client_socket.close()
                 break
 
-            message_obj = Message.get_from_socket(data)
-            if isinstance(message_obj, MediaPlayRequestMessage):
-                print('Received message to play media...')
-                filepath_of_media_to_play = message_obj.get_content()
-                for media in Media.get_all_from_db():
-                    if media.file_path == filepath_of_media_to_play:
-                        media_thread = threading.Thread(target=play_media, args=(media, client_socket, app))
-                        media_thread.start()
-            elif isinstance(message_obj, MediaStopRequestMessage):
-                print('Received message to stop playing media...')
-                filepath_of_media_to_stop = message_obj.get_content()
-                for media in currently_playing_media:
-                    if media.file_path == filepath_of_media_to_stop:
-                        media.stop()
-            elif isinstance(message_obj, MediaDeleteRequestMessage):
-                print('Received message to delete media...')
-                media_to_delete = Media.get_by_file_path(message_obj.media.file_path)
-                media_to_delete.delete(remove_related_cues=False)
-            elif isinstance(message_obj, MediaUploadRequestMessage):
-                print('Received message to upload media...')
-                Media.create(message_obj.file)
-                local_media = Media.get_all_from_db()
-                client_media_msg = ClientMediaDumpMessage(pickle.dumps(local_media))
-                client_media_msg.send(client_socket)
+            try:
+                if data_pieces:
+                    data = b''.join(data_pieces)
+                message_obj = Message.get_from_socket(data)
+                return message_obj
+            except (pickle.UnpicklingError, EOFError):
+                data_pieces.append(data)
+
+    def receive_server_messages():
+        message_obj = get_message_from_socket(client_socket)
+        if isinstance(message_obj, MediaPlayRequestMessage):
+            print('Received message to play media...')
+            filepath_of_media_to_play = message_obj.get_content()
+            for media in Media.get_all_from_db():
+                if media.file_path == filepath_of_media_to_play:
+                    media_thread = threading.Thread(target=play_media, args=(media, client_socket, app))
+                    media_thread.start()
+        elif isinstance(message_obj, MediaStopRequestMessage):
+            print('Received message to stop playing media...')
+            filepath_of_media_to_stop = message_obj.get_content()
+            for media in currently_playing_media:
+                if media.file_path == filepath_of_media_to_stop:
+                    media.stop()
+        elif isinstance(message_obj, MediaDeleteRequestMessage):
+            print('Received message to delete media...')
+            media_to_delete = Media.get_by_file_path(message_obj.media.file_path)
+            media_to_delete.delete(remove_related_cues=False)
+        elif isinstance(message_obj, MediaUploadRequestMessage):
+            print('Received message to upload media...')
+            Media.create(message_obj.file)
+            local_media = Media.get_all_from_db()
+            client_media_msg = ClientMediaDumpMessage(pickle.dumps(local_media))
+            client_media_msg.send(client_socket)
 
     receive_server_messages()
 
